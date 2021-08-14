8bef54ea816039e4110430d468603d3772281e9d
= <=>
2021-08-14 00:18:40 +0000

    Delete undo file remnant.

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 # When pulling down on a master for the first time, move files
 # from the git repo to the proper location.
+# (Or, for developing in repo, and moving updated files to salt master)
 cp /home/ubuntu/saltstates/* /srv/salt/
